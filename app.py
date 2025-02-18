06fe2d7198cf2b0a497b3f604ffcd3745164a807
faticoco <fb71459@gmail.com>
2025-02-18 13:57:30 +0500

    Update home route message in Flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Flask App at Vercel again"
 
 if __name__ == "__main__":
     app.run(debug=True)
